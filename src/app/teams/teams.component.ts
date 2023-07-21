5280cea5f3f86cc81ebd3a547fab6e3da45ed9c8
Dominik Ehrenberg <mail@dominik-ehrenberg.de>
2023-07-21 08:36:46 +0200

    Use player object

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,29 +1,33 @@
 import { Component } from '@angular/core';
 import { moveItemInArray, transferArrayItem, CdkDragDrop } from '@angular/cdk/drag-drop';
 
+interface Player {
+  name: string;
+}
+
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.scss'],
 })
 export class TeamsComponent {
-  team1 = [
-    'Tank 1',
-    'DPS 1',
-    'DPS 2',
-    'Support 1',
-    'Support 2',
+  team1: Array<Player> = [
+    { name: 'Tank 1'},
+    { name: 'DPS 1' },
+    { name: 'DPS 2' },
+    { name: 'Support 1' },
+    { name: 'Support 2' }
   ]
 
-  team2 = [
-    'Tank 2',
-    'DPS 3',
-    'DPS 4',
-    'Support 3',
-    'Support 4',
+  team2: Array<Player> = [
+    { name: 'Tank 2'},
+    { name: 'DPS 3' },
+    { name: 'DPS 4' },
+    { name: 'Support 3' },
+    { name: 'Support 4' }
   ]
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Player[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
